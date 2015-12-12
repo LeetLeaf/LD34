f15bd3752e969c7e2cd88b2e11a52d05f261e192
Kyle Keebler <kylekeebler@gmail.com>
2015-12-11 23:48:20 -0500

    Commit?

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour 
+{
+    public float influence;
+    public int health;
+    public float speed;
+
+    public string status;
+
+    public bool isLeader;
+    public bool isBoss;
+
+    public Transform leader;
+
+    public enum ClanType : int
+    { 
+        Good = 1,
+        Neutral = 0,
+        Evil = -1
+    };
+    public enum Aggerssion : int
+    {
+        Lawful = 1,
+        Neutral = 0,
+        Chaotic = -1
+    };
+	// Use this for initialization
+	void Start () 
+    {
+
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        switch (status)
+        { 
+            case "Follow":
+                Follow();
+                break;
+            default: //Idle
+
+                break;
+        }
+    }
+
+    public void Follow()
+    {
+        this.transform.position = Vector3.Lerp(this.transform.position, leader.position, speed/100);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Character : MonoBehaviour {
+public class Player : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
