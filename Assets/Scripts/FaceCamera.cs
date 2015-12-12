86fd7ad3600f692dc965ae6d7fe3e73c21eef5d9
Bryan Cantz <bryancantz@comcast.net>
2015-12-11 23:46:06 -0500

    Folder Structuring

---
+++ Assets/Scripts/FaceCamera.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceCamera : MonoBehaviour {
+
+    public bool fixedRotation = false;
+    public Sprite[] sprites;
+    private ViewDirection direction = ViewDirection.Front;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Transform spriteTransform = transform.FindChild("Sprite");
+
+        var lookPos = Camera.main.transform.position - transform.position;
+        lookPos.y = 0;
+        var rotation = Quaternion.LookRotation(lookPos);
+        spriteTransform.rotation = Quaternion.Slerp(spriteTransform.rotation, rotation, Time.deltaTime * 10);
+
+        if (fixedRotation)
+        {
+
+            var rotVal = (Quaternion.Inverse(transform.rotation) * spriteTransform.rotation).eulerAngles.y;
+            print(rotVal);
+
+            if (rotVal > 315 || rotVal < 45)
+            {
+                //Front
+                setDirection(ViewDirection.Front);
+            }
+            else if (rotVal >= 45 && rotVal <= 135) // Right Side
+            {
+                setDirection(ViewDirection.Right);
+            }
+            else if (rotVal >= 225 && rotVal <= 315)   //Left Side
+            {
+                setDirection(ViewDirection.Left);
+            }
+            else if (rotVal > 135 && rotVal < 225)
+            {
+                //Back
+                setDirection(ViewDirection.Back);
+            }
+
+        }
+	}
+
+    public void setDirection(ViewDirection direction)
+    {
+
+        transform.FindChild("Sprite").FindChild("Sprite_" + this.direction.ToString()).GetComponent<SpriteRenderer>().enabled = false;
+
+        this.direction = direction;
+
+        transform.FindChild("Sprite").FindChild("Sprite_" + this.direction.ToString()).GetComponent<SpriteRenderer>().enabled = true;
+    }
+
+    public enum ViewDirection{
+        Front,
+        Right,
+        Left,
+        Back
+    }
+}
---
+++ Assets/Scripts/SpriteShadow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteShadow : MonoBehaviour {
+
+    public bool castShadows;
+    public bool receiveShadows;
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<SpriteRenderer>().receiveShadows = receiveShadows;
+        this.GetComponent<SpriteRenderer>().castShadows = castShadows; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
